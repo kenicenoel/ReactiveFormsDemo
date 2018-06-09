279b47e08c2502c533b737391ef7d865711c03e5
Kenice Noel <kenicenoel@outlook.com>
2018-06-09 09:01:02 -0400

    Added dynamic form

--- src/app/array-form/array-form.component.ts
+++ src/app/array-form/array-form.component.ts
@@ -1,15 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, FormArray } from '@angular/forms';
 
 @Component({
-  selector: 'app-array-form',
+  selector: 'array-form',
   templateUrl: './array-form.component.html',
   styleUrls: ['./array-form.component.css']
 })
 export class ArrayFormComponent implements OnInit {
+  arrayForm: FormGroup;
+  constructor(private formBuilder: FormBuilder) { }
 
-  constructor() { }
+  ngOnInit() 
+  {
 
-  ngOnInit() {
+    
+    this.arrayForm = this.formBuilder.group(
+      {
+        email: '',
+        phones: this.formBuilder.array([]),
+      }
+    )
+
+    this.arrayForm.valueChanges.subscribe(console.log)
+  }
+
+  get phoneForms()
+  {
+    return this.arrayForm.get('phones') as FormArray
+  }
+
+  addNewPhone()
+  {
+    const phone = this.formBuilder.group({
+      area: [],
+      prefix: [],
+      number: []
+    })
+
+    this.phoneForms.push(phone);
+  }
+
+  deletePhone(i)
+  {
+    this.phoneForms.removeAt(i);
+
   }
 
 }
